c1b60fd134c7d97536a0abae1f3f5455e0021884
Cyp <cypher9715@naver.com>
2024-06-20 05:17:33 +0900

    intermediate commit

--- Source/14890.cpp
+++ Source/14890.cpp
@@ -9,36 +9,70 @@ public:
     
     bool constraint(const std::vector<int>& line)
     {
-        int index_last = line.size();
         int value_before = line[0];
         int point = 0;
 
-        for (int i = 1; i < index_last; ++i)
+        for (int i = 1; i < line.size(); ++i)
         {
-            int diff = abs(value_before - line[i]);
+            int diff = value_before - line[i];
 
-            if (diff != 0)
+            if (diff == 0)
             {
-                // 다음값과의 차이가 2이상일 경우 false.
-                if (diff > 1)
-                    return false;
+                continue;
+            }
+            
+            // 이전값과 현재값과의 차이가 2 이상일 경우 false
+            if (abs(diff) >= 2)
+                return false;
 
-                int temp = 0;
-
-                for (int j = i; j < L+i; ++j)
+            // 현재값이 이전 값보다 1 작을 경우.
+            if(diff == 1)
+            { 
+                // 만약 경사로를 세웠을 때 범위를 넘어서면 false.
+                if(i + L > line.size())
                 {
-                    temp += line[j];
+                    return false;
                 }
 
-                if (temp != (value_before * L))
+                // 경사로 point를 지정함.
+                if(i + L * 2 > line.size())
+                {
+                    point = line.size();
+                }
+                else
+                {
+                    point = i + L * 2;
+                }
+
+                // 경사로 point 가 line 과 같거나 크면 false, 겹치는것임.
+                for (int j = i; j < point; ++j)
+                {
+                    if(line[j] >= line[i])
+                    {
+                        return false;
+                    }
+                }
+
+                // 경사로가 여유 공간을 확보하고 있는지 확인.
+                for (int j = i; j < i + L; ++j)
+                {
+                    if(line[j] < line[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            // 현재값이 이전 값보다 1 클 경우.
+            else if(diff == -1)
+            {
+                if(i - L < 0)
                 {
                     return false;
                 }
             }
-
+        
             value_before = line[i];
         }
-
         return true;
     }
 
