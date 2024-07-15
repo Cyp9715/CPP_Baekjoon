eb99a26184eca62fdbb69b1dba67ccf73bb381a3
cypher9715 <cypher9715@naver.com>
2024-07-15 11:08:34 +0000

    update 1912

---
+++ Source/1912.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class baekjoon_1912
+{
+public:
+    int n;
+
+    void run()
+    {
+        // 10
+        // 2 1 -4 3 4 -4 6 5 -5 1
+
+        std::cin >> n;
+        std::vector<int> m(n);
+        std::vector<int> d(n, 0);
+        for (int& num : m) 
+            std::cin >> num;
+
+        d[0] = m[0]; // 첫번쨰 m 을 d[0] 에 할당.
+        int max_sum = d[0]; // max_sum 역시 d[0] 로 초기화.
+
+        // dynamic programming 의 핵심적인 역할을 수행하는 for 문
+        for(int i = 1; i < n; ++i)
+        {
+            // 만약 m[i]가 더 크다면, 새로운 연속 구간을 시작.
+            // 만약 d[i-1] + m[i]가 더 크다면, 이전 연속 구간을 계속 유지.
+            d[i] = std::max(m[i], d[i-1] + m[i]); // d[i]는 단기적인 최고값을 보유
+            max_sum = std::max(max_sum, d[i]); // max_sum 은 최종적인 최고값을 보유
+        }
+
+        std::cout << max_sum << std::endl;
+    }
+};
